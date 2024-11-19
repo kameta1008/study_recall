9811c0dbcc08e9c41ca200e30de2973ec1486765
kameta1008 <takemarue202@gmail.com>
2024-11-19 17:28:34 +0900

    studyモデルのテストコード

--- spec/factories/studies.rb
+++ spec/factories/studies.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :study do
-    title { "MyString" }
-    content { "MyText" }
-    user { nil }
+    title { Faker::Lorem.sentence(word_count: 3) } 
+    content { Faker::Lorem.paragraph(sentence_count: 2) } 
+    association :user 
+
+    after(:build) do |study|
+      if File.exist?('public/images/test_image.png')
+        study.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      end
+    end
   end
 end
--- spec/models/study_spec.rb
+++ spec/models/study_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Study, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @study = FactoryBot.build(:study)
+  end
+
+  describe '学習記録の投稿' do
+    context '投稿ができる場合' do
+      it '画像、タイトル、テキストがすべて入力されていれば投稿できる' do
+        expect(@study).to be_valid
+      end
+
+      it '画像がなくても投稿できる' do
+        @study.image = nil
+        expect(@study).to be_valid
+      end
+    end
+
+    context '投稿ができない場合' do
+      it 'テキストが空では投稿できない' do
+        @study.content = ''
+        @study.valid?
+        expect(@study.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it 'タイトルが空では投稿できない' do
+        @study.title = ''
+        @study.valid?
+        expect(@study.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'ユーザーが紐づいていなければ投稿できない' do
+        @study.user = nil
+        @study.valid?
+        expect(@study.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
